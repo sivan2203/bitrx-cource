0995f9981573ed4fbe717bf4710688b352bc0bc0
sivan2203 <sivan9458@gmail.com>
2021-06-03 13:16:16 +0700

    Зашел спустя время чтобы закончить курс

--- web/bitrix/managed_cache/MYSQL/b_group/ca/ca3bd4e78eedfb28384c3bdea65760bb.php
+++ web/bitrix/managed_cache/MYSQL/b_group/ca/ca3bd4e78eedfb28384c3bdea65760bb.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001622095527';
-$dateexpire = '001622099127';
+$datecreate = '001622700873';
+$dateexpire = '001622704473';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:15:"SECURITY_POLICY";N;}}';
 return true;
 ?>
---
+++ web/bitrix/managed_cache/MYSQL/menu/1c/1cf25d29bb4eaea7fa5ae35ffcfb0804.php
@@ -0,0 +1,7 @@
+<?
+if($INCLUDE_FROM_CACHE!='Y')return false;
+$datecreate = '001622700873';
+$dateexpire = '001622704473';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:6:{i:0;a:3:{s:4:"LINK";s:1:"/";s:5:"LINKS";a:1:{i:0;s:1:"/";}s:4:"PERM";s:1:"R";}i:1;a:3:{s:4:"LINK";s:7:"/about/";s:5:"LINKS";a:1:{i:0;s:7:"/about/";}s:4:"PERM";s:1:"R";}i:2;a:3:{s:4:"LINK";s:10:"/services/";s:5:"LINKS";a:1:{i:0;s:10:"/services/";}s:4:"PERM";s:1:"R";}i:3;a:3:{s:4:"LINK";s:11:"/portfolio/";s:5:"LINKS";a:1:{i:0;s:11:"/portfolio/";}s:4:"PERM";s:1:"R";}i:4;a:3:{s:4:"LINK";s:6:"/blog/";s:5:"LINKS";a:1:{i:0;s:6:"/blog/";}s:4:"PERM";s:1:"R";}i:5;a:3:{s:4:"LINK";s:10:"/contacts/";s:5:"LINKS";a:1:{i:0;s:10:"/contacts/";}s:4:"PERM";s:1:"R";}}}';
+return true;
+?>
